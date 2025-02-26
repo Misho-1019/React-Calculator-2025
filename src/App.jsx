7f8ac8d59a61d4bcf6f45a5d0c16b4c368fb6e92
Misho1019 <mikailtorres99@gmail.com>
2025-02-26 06:44:54 +0200

    Added calculator structure and css

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,32 @@ function App() {
 
   return (
     <>
-    
+      <div class="calculator">
+        <input type="text" id="display" class="display" disabled />
+        <div class="buttons">
+          <button class="button" onclick="appendNumber('7')">7</button>
+          <button class="button" onclick="appendNumber('8')">8</button>
+          <button class="button" onclick="appendNumber('9')">9</button>
+          <button class="button operator" onclick="appendOperator('/')">/</button>
+
+          <button class="button" onclick="appendNumber('4')">4</button>
+          <button class="button" onclick="appendNumber('5')">5</button>
+          <button class="button" onclick="appendNumber('6')">6</button>
+          <button class="button operator" onclick="appendOperator('*')">*</button>
+
+          <button class="button" onclick="appendNumber('1')">1</button>
+          <button class="button" onclick="appendNumber('2')">2</button>
+          <button class="button" onclick="appendNumber('3')">3</button>
+          <button class="button operator" onclick="appendOperator('-')">-</button>
+
+          <button class="button" onclick="appendNumber('0')">0</button>
+          <button class="button" onclick="appendDot()">.</button>
+          <button class="button equals" onclick="calculateResult()">=</button>
+          <button class="button operator" onclick="appendOperator('+')">+</button>
+        </div>
+        <button class="clear" onclick="clearDisplay()">AC</button>
+      </div>
+
     </>
   )
 }
